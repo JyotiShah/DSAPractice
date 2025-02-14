f2db24dc8ff98acec9f40408b4bb14c4240f9230
unknown <jyoti.shah9753@gmail.com>
2025-02-14 22:35:59 +0530

    Changed java version

--- src/main/java/Assignment1Arrays/Q2.java
+++ src/main/java/Assignment1Arrays/Q2.java
@@ -1,4 +1,10 @@
 package Assignment1Arrays;
 
 public class Q2 {
+    public static void main(String[] args){
+        int a = 10, b = 20;
+        if(!(a > 10 && b > 20))
+            System.out.println(a+" "+b+" ");
+        System.out.println(a+" "+b);
+    }
 }
--- src/main/java/Assignment1Arrays/Q3.java
+++ src/main/java/Assignment1Arrays/Q3.java
@@ -1,4 +1,8 @@
 package Assignment1Arrays;
 
 public class Q3 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
