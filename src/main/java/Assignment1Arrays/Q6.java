eec01ec72a26ef66fdfeed15f8666fc325e44c51
Jyoti Shah <jyoti.shah9753@gmail.com>
2025-08-18 22:29:46 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/Inheritance/Calling.java
    #	src/main/java/Inheritance/Child1.java
    #	target/classes/Inheritance/Calling.class
    #	target/classes/Inheritance/Child1.class

--- src/main/java/Assignment1Arrays/Q6.java
+++ src/main/java/Assignment1Arrays/Q6.java
@@ -8,7 +8,18 @@ public class Q6 {
 
     public static void main(String[] args) {
 
+        String str = "*^*&%&$# djkn(&^skfdsl&*^*%$ sdfsd";
+        System.out.println(str.replaceAll("[^a-zA-Z0-9]",""));
+        System.out.println(nextChar("Vijay"));
     }
 
+    public static String nextChar(String str) {
+        String replaced = "";
+        for(int i=0; i<=str.length()-1; i++){
+            char c = (char) (str.charAt(i)+1);
+            replaced = replaced + c;
+        }
+        return replaced;
+    }
 
 }
